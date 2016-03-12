68eaca1d286a389c000ff7c2c41dfe90f9031201
danielpowell4 <danielpowell12@msn.com>
2016-03-11 20:32:01 -0700

    install devise

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module BeaconPin
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #added with devise and heroku
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TODO set to actual host is ever moved
+  config.action_mailer.default_url_options = { host: 'http://beacon-pin.herokuapp.com/' }
 end
