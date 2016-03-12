f30cc13c4eb1939bbefe392278b598f26809000f
danielpowell4 <danielpowell12@msn.com>
2016-03-11 22:55:01 -0700

    images with paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,4 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://56.media.tumblr.com/170b376be332902fd8365a6db73b4164/tumblr_nyv6efMxPM1udod9xo1_1280.jpg"
 end
