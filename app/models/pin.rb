32a4ca1ab7d72209f7a56c30fc86b3e21624a919
danielpowell4 <danielpowell12@msn.com>
2016-03-13 14:23:33 -0600

    update pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: "http://56.media.tumblr.com/170b376be332902fd8365a6db73b4164/tumblr_nyv6efMxPM1udod9xo1_1280.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   validates :image, presence: true
   validates :description, presence: true
-  
+
 end
